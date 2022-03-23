7ba584891e33fa9ca94dd1969a1bc407bc9261b0
Kasidate P <88122986+Kasidatep@users.noreply.github.com>
2022-03-23 15:42:47 +0700

    web landing page

---
+++ web-assets/js/scripts.js
@@ -0,0 +1,6 @@
+$(function () {
+    // init feather icons
+    feather.replace();
+});
+let year = new Date().getFullYear();
+document.getElementById("year").innerHTML ='COPYRIGHT © ' + year + ' KASIDATE.ME - KASIDATEPLUS';
