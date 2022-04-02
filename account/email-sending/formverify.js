cd4a4ba1328ef2a488046756089d4e12d92e7fdf
kasidatep <kasidate.p@mail.kmutt.ac.th>
2022-04-02 13:05:29 +0700

    verify acc otp

--- account/email-sending/formverify.js
+++ account/email-sending/formverify.js
@@ -2,6 +2,18 @@ let mail = "testing@temp.kasidate.me";
 let password = "12345678";
 let sendotp;
 let sendotpid;
+let smssendotp;
+let smssendotpid;
+let globalpassword;
+let globalusername;
+let globalmail;
+let otp = makeotp(6);
+let otpid = makeotpid(4);
+let smsotp = makeotp(6);
+let smsotpid = makeotpid(4);
+let usernameiduniqe = makeusernameiduniqe(2);
+let usernamefornull = makeusernameiduniqe(8);
+let passwordfornull = makeusernameiduniqe(8);
 
 function makeotpid(length) {
     var otpidresult = '';
@@ -14,10 +26,6 @@ function makeotpid(length) {
     return otpidresult;
 }
 
-let otpid = makeotpid(4);
-
-document.getElementById('otpid').innerHTML = otpid;
-
 function makeotp(length) {
     var otpresult = '';
     var otpcharacters = '0123456789';
@@ -29,14 +37,16 @@ function makeotp(length) {
     return otpresult;
 }
 
-let otp = makeotp(6);
-
-document.getElementById('otppassword').innerHTML = otp;
-
-sendotpid = otpid;
-sendotp = otp;
-document.getElementById('sendotpidref').innerHTML = sendotpid;
-document.getElementById('sendotpid').innerHTML = sendotpid;
+function makeusernameiduniqe(length) {
+    var usernameiduniqeresult = '';
+    var usernameiduniqecharacters = 'abcdefghijklmnopqrstuvwxyz0123456789';
+    var usernameiduniqecharactersLength = usernameiduniqecharacters.length;
+    for (var i = 0; i < length; i++) {
+        usernameiduniqeresult += usernameiduniqecharacters.charAt(Math.floor(Math.random() *
+            usernameiduniqecharactersLength));
+    }
+    return usernameiduniqeresult;
+}
 
 function KasPActionOTPBlock() {
     location.reload();
@@ -53,58 +63,78 @@ function otpTrackFunction() {
     }
 }
 
-function makeusernameiduniqe(length) {
-    var usernameiduniqeresult = '';
-    var usernameiduniqecharacters = 'abcdefghijklmnopqrstuvwxyz0123456789';
-    var usernameiduniqecharactersLength = usernameiduniqecharacters.length;
-    for (var i = 0; i < length; i++) {
-        usernameiduniqeresult += usernameiduniqecharacters.charAt(Math.floor(Math.random() *
-            usernameiduniqecharactersLength));
+function smsotpTrackFunction() {
+    var smstrackresult = document.getElementById("smsotpforminput").value;
+    if (smstrackresult == null) {
+        document.getElementById("smstrackresult").innerText = "*****";
+    } else if (smssendotp === smstrackresult) {
+        document.getElementById("smstrackchecker").innerText = "welcome";
+    } else {
+        document.getElementById("smstrackchecker").innerText = "try again";
     }
-    return usernameiduniqeresult;
 }
-
-let usernameiduniqe = makeusernameiduniqe(2);
-let usernamefornull = makeusernameiduniqe(8);
-let passwordfornull = makeusernameiduniqe(8);
-let globalpassword;
-let globalusername;
-let globalmail;
 
 function regismailpassword() {
     var getuservalue = document.getElementById("usernameregis").value;
     var getusernameregis
-    if (getuservalue === "") {
-        getusernameregis = usernamefornull;
-    } else {
-        getusernameregis = getuservalue;
-    };
+    if (getuservalue === "") 
+        {
+            getusernameregis = usernamefornull;
+        } else {
+            getusernameregis = getuservalue;
+        };
 
     var getpasswordvalue = document.getElementById("passwordregis").value;
     var getpasswordregis
-    if (getpasswordvalue === "") {
-        getpasswordregis = passwordfornull;
-        document.getElementById("passwordregis").value = passwordfornull;
-        document.getElementById("passwordvaluedisplay").innerText = passwordfornull;
-    } else {
-        getpasswordregis = getpasswordvalue;
-    };
+    if (getpasswordvalue === "") 
+        {
+            getpasswordregis = passwordfornull;
+            document.getElementById("passwordregis").value = passwordfornull;
+            document.getElementById("passwordvaluedisplay").innerText = passwordfornull;
+        } 
+        else {
+            getpasswordregis = getpasswordvalue;
+        };
+
     globalusername = getusernameregis;
     globalpassword = getpasswordregis;
 
+    var mailmixing = getusernameregis + "." + usernameiduniqe + "@temp.kasidate.me";
+    document.getElementById("mailregis").value = mailmixing;
+    globalmail = mailmixing;
+}
 
-    var sa = getusernameregis + "." + usernameiduniqe + "@temp.kasidate.me";
-    document.getElementById("mailregis").value = sa;
-    globalmail = sa;
+function loginmailpassword() {
+    var usernamelogin = document.getElementById("usernamelogin").value;
+    var passwordlogin = document.getElementById("passwordlogin").value;
+    var smsotpforminput = document.getElementById("smsotpforminput").value;
+
+    console.log(usernamelogin);
+    console.log(passwordlogin);
+    console.log(smsotpforminput);
+
+    if(usernamelogin === globalusername && passwordlogin === globalpassword && smsotpforminput === smsotp){
+        document.getElementById('loginstatus').innerText = "Log In Successfully";
+    } else { document.getElementById('loginstatus').innerText = "filed Log in";}
 }
 
 function copyfunction() {
     navigator.clipboard.writeText(document.getElementById("mailregis").value);
     document.getElementById("actioncopieddisplay").innerText = "Copied the text: " + document.getElementById("mailregis").value;
-    console.log(globalpassword);
-    console.log(globalusername);
-
     document.getElementById('mail').innerText = globalmail;
     document.getElementById('showpassword').innerText = globalpassword;
     document.getElementById('mailshow').innerText = globalmail;
 }
+
+sendotpid = otpid;
+sendotp = otp;
+smssendotpid = smsotpid; 
+smssendotp = smsotp;
+
+document.getElementById('otpid').innerHTML = otpid;
+document.getElementById('otppassword').innerHTML = otp;
+document.getElementById('sendotpidref').innerHTML = sendotpid;
+document.getElementById('sendotpid').innerHTML = sendotpid;
+document.getElementById('smsotpid').innerHTML = smsotpid;
+document.getElementById('smsotppassword').innerHTML = smsotp;
+document.getElementById('smssendotpidref').innerHTML = smssendotpid;
